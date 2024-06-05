08b43b76c051267ffd91550bf8ead54723fdaa60
Amaury Pelzer <amaury.pelzer@gmail.com>
2024-06-05 11:56:24 +1000

    Added script to copy relevant MRIs to HPC

---
+++ scripts/copy_MRI.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set the path to your CSV file
+CSV_FILE="../data/processed/MRI_file_path.csv"
+HPC_USERNAME="apelzer"
+HPC_HOST="m3.massive.org.au"
+HPC_DEST_DIR="~/vn36_scratch/apelzer/engel_mri"
+
+# Read the CSV file line by line
+echo "Starting the file copy process..."
+tail -n +2 $CSV_FILE | while IFS=',' read -r ParticipantID surg_engel ScanPath
+do
+  # Remove carriage return character, the extra directory level from the ScanPath, and change \ to /
+  CORRECTED_PATH=$(echo $ScanPath | tr -d '\r' | sed 's|\.\./||' | sed 's|\\|/|g')
+
+  # Check if the ScanPath is not empty
+  if [[ -n "$CORRECTED_PATH" ]]; then
+    echo "Copying file for ParticipantID $ParticipantID: $CORRECTED_PATH"
+    # Copy the file to the HPC
+    scp $CORRECTED_PATH ${HPC_USERNAME}@${HPC_HOST}:${HPC_DEST_DIR}
+    
+    if [[ $? -eq 0 ]]; then
+      echo "Successfully copied $CORRECTED_PATH to ${HPC_DEST_DIR}"
+    else
+      echo "Failed to copy $CORRECTED_PATH"
+    fi
+  else
+    echo "No file path for ParticipantID $ParticipantID"
+  fi
+done
+echo "File copy process completed."
